d804c543b9af8550fabbe1d7ba93e5587fbc7715
CptDoraemon <yxx0827@gmail.com>
2019-06-07 14:27:38 -0400

    Updates

--- precache-manifest.c61562e9c312c5c7dfd9802f08a9ca6c.js
+++ precache-manifest.c61562e9c312c5c7dfd9802f08a9ca6c.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/youtube-downloader/static/js/runtime~main.70d9d589.js"
   },
   {
-    "revision": "ef2a19d42e21d2b54241",
-    "url": "/youtube-downloader/static/js/main.2229503c.chunk.js"
+    "revision": "877e630b4bb59cf408e4",
+    "url": "/youtube-downloader/static/js/main.d23af1f4.chunk.js"
   },
   {
     "revision": "e1df9e5dd97f9dc5b41a",
     "url": "/youtube-downloader/static/js/2.373ccd69.chunk.js"
   },
   {
-    "revision": "ef2a19d42e21d2b54241",
-    "url": "/youtube-downloader/static/css/main.5d10fd08.chunk.css"
+    "revision": "877e630b4bb59cf408e4",
+    "url": "/youtube-downloader/static/css/main.311fa538.chunk.css"
   },
   {
-    "revision": "f4bd05067394b0efc1b3128074d7c3a1",
+    "revision": "406f2c67f39685d05d21c06f54d78f6a",
     "url": "/youtube-downloader/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/youtube-downloader/precache-manifest.43f510576a1314e73d748ac36dec1ce5.js"
+  "/youtube-downloader/precache-manifest.c61562e9c312c5c7dfd9802f08a9ca6c.js"
 );
 
 workbox.clientsClaim();
